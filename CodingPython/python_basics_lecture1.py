3816797689bdb8764f2265342d5f82d6cfccebf2
beki255 <bereket2553@gmail.com>
2026-05-02 11:08:48 +0300

    Add python_basics_lecture1.py

---
+++ CodingPython/python_basics_lecture1.py
@@ -0,0 +1,20 @@
+'''print ("First line\nSecond line")
+     
+Ask for the user’s first name and 
+display the output message
+name=input("please enter your first name")
+print("hello",name) '''
+print("bereket",end=" ")
+print('sahlemariam')
+print("python","cpp")
+print("python","cpp",sep='####')
+a=200
+b=13
+print(a/b)
+print(format(a/b,'.3f'))
+# inserting comma separators
+print(format(2354,',f'))
+print(format(23,'0%'))
+print(format(2354,',d'))
+result = 2 + 5 * 6 / 2  #2 + (5 * 6) / 2
+print(result) # Output: 17.0
