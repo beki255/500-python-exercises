fa364f45b68528c04038cb93efd65cc3b5eb7a00
beki255 <bereket2553@gmail.com>
2026-05-02 11:06:52 +0300

    Add basic_functions_demo.py

---
+++ CodingPython/basic_functions_demo.py
@@ -0,0 +1,58 @@
+#1
+def greetings (name):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+print(greetings('Asabeneh'))
+#2
+def add_ten(num):
+   ten = 10
+   return num + ten
+print(add_ten(90))
+#3
+def square_number(x):
+  return x * x
+print(square_number(2))
+#4
+def area_of_circle (r):
+   PI = 3.14
+   area = PI * (r ** 2)
+   return area
+print(area_of_circle(10))
+
+#5
+def sum_of_numbers(n):
+   total = 0
+   for i in range(n+1):
+       total+=i
+   return total
+print(sum_of_numbers(10))
+print(sum_of_numbers(100))
+
+#6
+def weight_of_object (mass, gravity):
+    weight = str(mass * gravity)+ ' N' # the value has to be changed to a string first
+    return weight
+print('Weight of an object in Newtons: ', weight_of_object(100, 9.81))    
+
+#7 
+def print_fullname(firstname, lastname):
+   space = ' '
+   full_name = firstname + space + lastname
+   print(full_name)
+print_fullname( lastname='Yetayeh' , firstname = 'Asabeneh') #  order does not matter
+#8
+def add_two_numbers (num1, num2):
+   total = num1 + num2
+   print(total)
+add_two_numbers(num2 = 3, num1 = 2) # Order does not matter
+
+
+#9 Returning a boolean: Example:
+def is_even (n):
+   if n % 2 == 0:
+      print('even')
+   else:
+       print("odd")   
+   return True                             # return stops further execution of the function, similar to break
+print(is_even(10)) # True
+print(is_even(7)) # False
