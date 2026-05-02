4e6b65d8efa6eef2344edb6a0d2c7b21453c67fe
beki255 <bereket2553@gmail.com>
2026-05-02 11:06:52 +0300

    Add compare_two_numbers.py

---
+++ CodingPython/compare_two_numbers.py
@@ -0,0 +1,17 @@
+'''x=int(input("x="))
+  if x<0:
+  print("negative")
+  elif x>0:
+      print("postive")
+  else:
+          print("zero")'''
+
+ num1=int(input("enter first number"))
+ num2=int(input("enter second number"))
+ if num1>num2:
+  print(" num1 is the largest number")
+ elif num1<num2:
+  print(" num2 is the largest number")
+ else:
+  print("they are equal")
+
